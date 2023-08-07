a4268c5f2f7fcf6cc3e1adc9fa64996711e9d11c
Tien <manhtien790@gmail.com>
2023-08-08 01:55:23 +0700

    lý do thêm code

--- src/modules/form-sinh-vien/formSinhVien.jsx
+++ src/modules/form-sinh-vien/formSinhVien.jsx
@@ -2,33 +2,45 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 class FormSinhVien extends Component {
   state ={
-    values:{
+    value:{
       maSV:'',
       hoTen:'',
       email:'',
       soDienThoai:'',
     },
-    errors:{
-      maSV:'',
-      hoTen:'',
-      email:'',
-      soDienThoai:'',
-    }
+    error:{
+      maSV:'maSV Khong duoc bo trong',
+      hoTen:'hoTen Khong duoc bo trong',
+      email:'email Khong duoc bo trong',
+      soDienThoai:'soDienThoai Khong duoc bo trong',
+    },
+    touch:{
+      maSV:false,
+      hoTen:false,
+      email:false,
+      soDienThoai:false,
+    },
+  };
+  handleChange =(event) =>{
+    const {value,maSV,hoTen,email,soDienThoai, name} = event.target;
+    this.setState({
+      value:{
+        ...this.state.value,
+        [name]:value,
+      },
+    });
   }
-  handleChange = (event) =>{
-    let tagInput = event.target;
-    let {name,value} = tagInput;
-    // this.setState ({
-    //   [name]:value
-    // })
-    if(value === ''){
-    }
-  }
-  handleSubmit = (event) =>{
-    event.prevenDefault();
-    this.props.themSinhVien(this.state)
+  handleForcus = (event)=>{
+    const {name} = event.target;
+    this.setState({
+      touch:{
+        ...this.state.touch,
+        [name]:true,
+      },
+    });
   }
   render() {
+    console.log(this.state.touch)
     return (
     <div className='container'> 
       <div className="card text-left">
@@ -40,25 +52,54 @@ class FormSinhVien extends Component {
             <div className="row">
               <div className="form-group col-6">
                 <span>Ma Sinh Vien</span>
-                <input type='text'  className='form-control'/>
-                <p className='text-danger'>{this.state.errors.maSV}</p>
+                <input 
+                onFocus={this.handleForcus}
+                value={this.state.value.maSV} 
+                onChange={this.handleChange}
+                name='maSV'
+                className='form-control'/>
+                {this.state.touch.name && this.state.error.name &&
+                (<p className='text-danger'> Khong duoc bo trong</p>)
+                }
+                
               </div>
               <div className="form-group col-6">
                 <span>Ho va Ten</span>
-                <input type='text' className='form-control' />
-                <p className='text-danger'>{this.state.errors.hoTen}</p>
+                <input
+                onFocus={this.handleForcus}
+                value={this.state.value.hoTen}  
+                onChange={this.handleChange}
+                name='hoTen'
+                className='form-control' />
+                {this.state.touch.name && this.state.error.name &&
+                (<p className='text-danger'> Khong duoc bo trong</p>)
+                }
               </div>
             </div>
             <div className="row">
               <div className="form-group col-6">
                 <span>So Dien Thoai</span>
-                <input type='text' className='form-control' />
-                <p className='text-danger'>{this.state.errors.email}</p>
+                <input 
+                onFocus={this.handleForcus}
+                value={this.state.value.soDienThoai} 
+                onChange={this.handleChange}
+                name='soDienThoai'
+                className='form-control' />
+                {this.state.touch.name && this.state.error.name &&
+                (<p className='text-danger'> Khong duoc bo trong</p>)
+                }
               </div>
               <div className="form-group col-6">
                 <span>Email</span>
-                <input type='text' className='form-control'/>
-                <p className='text-danger'>{this.state.errors.soDienThoai}</p>
+                <input 
+                onFocus={this.handleForcus}
+                value={this.state.value.email} 
+                onChange={this.handleChange}
+                name='email'
+                className='form-control'/>
+                {this.state.touch.name && this.state.error.name &&
+                (<p className='text-danger'> Khong duoc bo trong</p>)
+                }
               </div>
             </div>
             <div className="row my-2">
